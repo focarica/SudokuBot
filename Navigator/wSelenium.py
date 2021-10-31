0b2f49dcf017b58a4ca49921637ab6cbcdd072e7
Foca <a.vinicius0042@gmail.com>
2021-10-31 18:35:12 -0300

    Config selenium part

--- Navigator/wSelenium.py
+++ Navigator/wSelenium.py
@@ -1,9 +1,29 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
+LINK = "https://sudokutable.com"
+HEADER = {
+    'User-Agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 OPR/80.0.4170.72'
+}
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+options.add_experimental_option("detach", True)
 class WSelenium:
     def __init__(self):
-        driver = webdriver.Chrome()
-        driver.get("https://sudokutable.com")
+        
+        driver = webdriver.Chrome(options=options)
 
-WSelenium()
+        driver.get(LINK)
+        driver.set_window_size(1110, 768)
+
+        driver.find_element_by_id('agree_cookies').click()
+        driver.find_element_by_class_name('dropdown').click()
+        driver.find_element_by_xpath('//*[@id="difficulty"]/ul/li[4]').click()
+
+        self.url = driver.page_source
+
+    def BeautifulSoup(self):
+        bs = BeautifulSoup(self.url, 'html.parser')
+        return bs
